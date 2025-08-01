c47d92af88dee353f2bc57c90bced03eb5460d83
louner17 <bbartekpawlowski@gmail.com>
2025-08-01 12:44:25 +0200

    test2

--- main_prod.py
+++ main_prod.py
@@ -1,7 +1,14 @@
-# plik: main_prod.py (Test 1 - absolutne minimum)
+# plik: main_prod.py (Test 2 - dodajemy auth)
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+import os
+from app.auth.router import router as auth_router
 
 app = FastAPI()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
+app.include_router(auth_router)
 
 @app.get("/")
 def read_root():
